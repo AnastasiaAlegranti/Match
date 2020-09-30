bc39ad8d1c42d962ebf5a24f67d24af95c5afa29
Anastasia Alegranti <anastasia@comax.co.il>
2020-10-01 01:59:59 +0300

    remove unnecessary imports from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,11 @@ import {HomeComponent} from './components/home/home.component';
 import {MatchDetailsComponent} from './components/match-details/match-details.component';
 import {LineUpComponent} from './components/line-up/line-up.component';
 import {SubstitutionsComponent} from './components/substitutions/substitutions.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {FormsModule} from '@angular/forms';
 import {SelectPlayerComponent} from './components/select-player/select-player.component';
-import {MatInputModule} from '@angular/material/input';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatRadioModule} from '@angular/material/radio';
-import {MatTabsModule} from '@angular/material/tabs';
 import {SelectSubstitutionPlayerComponent} from './components/select-substitution-player/select-substitution-player.component';
 
 @NgModule({
@@ -33,7 +30,7 @@ import {SelectSubstitutionPlayerComponent} from './components/select-substitutio
     ],
     entryComponents: [],
     imports: [
-        BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, MatFormFieldModule, MatSelectModule, FormsModule, MatInputModule, MatToolbarModule, MatRadioModule, MatTabsModule],
+        BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, MatSelectModule, FormsModule, MatToolbarModule, MatRadioModule],
     providers: [
         StatusBar,
         SplashScreen,
